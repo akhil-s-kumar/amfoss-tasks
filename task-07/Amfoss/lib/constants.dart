cec23958306d560b8b5ec461dabee142ec369152
Akhil S Kumar <54428228+akhil-s-kumar@users.noreply.github.com>
2020-11-29 20:13:48 +0530

    Add files via upload

---
+++ task-07/Amfoss/lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF703AFB);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF703AFB), Color(0xFF703AFB)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+const kAnimationDuration = Duration(microseconds: 200);
