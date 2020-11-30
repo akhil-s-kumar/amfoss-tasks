250bcd0410f94d615c62d730a9890ae730a40c3f
Akhil S Kumar <54428228+akhil-s-kumar@users.noreply.github.com>
2020-11-30 12:05:47 +0530

    Update main.go

--- task-06/main.go
+++ task-06/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/vartanbeno/go-reddit/reddit"
 )
@@ -11,31 +12,47 @@ import (
 var ctx = context.Background()
 
 func main() {
-
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func run() (err error) {
-	withCredentials := reddit.WithCredentials("id", "secrete", "username", "password")
-	client, _ := reddit.NewReadonlyClient(withCredentials)
-	// Let's get the top 200 posts of r/golang.
-	// Reddit returns a maximum of 100 posts at a time,
-	// so we'll need to separate this into 2 requests.
-	posts, _, err := reddit.DefaultClient().Subreddit.TopPosts(ctx, "memes", &reddit.ListPostOptions{
+
+	withCredentials := reddit.WithCredentials("##########", "########################", "###########", "##########")
+	client, _ := reddit.NewClient(withCredentials)
+	if err != nil {
+		return
+	}
+
+	client.OnRequestCompleted(logResponse)
+
+	subreddits, _, err := reddit.DefaultClient().Subreddit.SearchNames(ctx, "memes")
+	fmt.Println(subreddits)
+
+	sr, _, err := reddit.DefaultClient().Subreddit.Get(ctx, "memes")
+	fmt.Printf("%s was created on %s and has %d subscribers.\n", sr.NamePrefixed, sr.Created.Local(), sr.Subscribers)
+
+	posts, _, err := client.Subreddit.TopPosts(context.Background(), "memes", &reddit.ListPostOptions{
 		ListOptions: reddit.ListOptions{
 			Limit: 100,
 		},
 		Time: "week",
 	})
-
 	if err != nil {
-		return
+		return err
 	}
+
 	for _, post := range posts {
-		fmt.Println(post.Title)
-		client.Post.Upvote(context.Background(), post.ID)
+		_, err = client.Post.Upvote(context.Background(), post.FullID)
+		if err != nil {
+			return err
+		}
 	}
+
 	return
+}
+
+func logResponse(req *http.Request, res *http.Response) {
+	fmt.Printf("%s %s %s\n", req.Method, req.URL, res.Status)
 }
